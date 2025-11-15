20ba5e87a7055b299506223bccbcfe2879ee7398
Jordan Fields <74157993+Nadr0jj@users.noreply.github.com>
2025-11-14 21:16:40 -0800

    add request-level caching to thick client

--- src/osrs_prices_client/client/realtime_prices_thick_client.py
+++ src/osrs_prices_client/client/realtime_prices_thick_client.py
@@ -1,5 +1,8 @@
+from functools import lru_cache
+
 import pandas as pd
 from requests import Response
+
 from .realtime_prices_client import RealtimePricesClient
 from ..model.realtime_prices_request import RealtimePricesRequest
 from ..model.timestep import Timestep
@@ -9,8 +12,15 @@ from ..model.interpolation_method import InterpolationMethod
 class RealtimePricesThickClient:
     """Provides additional functionality around RealtimePricesClient methods"""
 
-    def __init__(self, realtime_prices_client: RealtimePricesClient):
+    def __init__(
+        self,
+        realtime_prices_client: RealtimePricesClient,
+        *,
+        cache_enabled: bool = True,
+    ):
         self.realtime_prices_client = realtime_prices_client
+        self._cache_enabled = cache_enabled
+        self._cached_fetch_item_frame = lru_cache(maxsize=None)(self._fetch_item_frame_uncached)
 
     def _parse_response(self, item_id: str, response: Response) -> pd.DataFrame:
         df = pd.DataFrame(response.json()["data"])
@@ -23,11 +33,20 @@ class RealtimePricesThickClient:
         # pylint: disable-next=protected-access
         return self.realtime_prices_client._call_endpoint(item_id, timestep)
 
+    def _fetch_item_frame_uncached(self, item_id: str, timestep: Timestep) -> pd.DataFrame:
+        return self._parse_response(item_id, self._request(item_id, timestep))
+
+    def clear_cache(self) -> None:
+        """Clears the cached per-item frames."""
+        self._cached_fetch_item_frame.cache_clear()
+
+    def _fetch_item_frame(self, item_id: str, timestep: Timestep) -> pd.DataFrame:
+        if not self._cache_enabled:
+            return self._fetch_item_frame_uncached(item_id, timestep)
+        return self._cached_fetch_item_frame(item_id, timestep)
+
     def get_prices(self, request: RealtimePricesRequest) -> pd.DataFrame:
-        dfs = [
-            self._parse_response(item_id, self._request(item_id, request.timestep))
-            for item_id in request.item_ids
-        ]
+        dfs = [self._fetch_item_frame(item_id, request.timestep) for item_id in request.item_ids]
         concatenated_df = pd.concat(dfs, axis=1, join="outer")
 
         if request.interpolation_method == InterpolationMethod.LINEAR:
